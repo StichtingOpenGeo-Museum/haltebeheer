8dc046cd777f280b56085a00da36df16984f8189
Stefan de Konink <stefan@konink.de>
2011-11-27 22:25:07 +0100

    Connexxion import script. Reads openOV scraper dump from CXX.

---
+++ haltes/stops/load_cxx.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import simplejson as json
+from django.contrib.gis.geos import *
+from models import Stop, StopAttribute, Agency, AgencyAttribute
+
+def run(verbose=True):
+    f = codecs.open(sys.argv[1], encoding='utf-8', mode='r')
+    content = f.read()
+    f.close()
+    content = '{'+content.split('>{')[1].split('}<')[0]+'}'
+    j = json.loads(content)
+
+    a = Agency.objects.filter(name='Connexxion')
+    for i in range(1, len(j['HALTELIST']['ID'])):
+        common_city = j['HALTELIST']['NAAM'][i].rplace(', '+j['HALTELIST']['KORTENAAM'])
+        
+        pnt = Point(j['HALTELIST']['LONGITUDE'][i]/10000000.0,
+                    j['HALTELIST']['LATITUDE'][i]/10000000.0, srid=4326)
+        
+        s = Stop(tpc=j['HALTELIST']['ID'][i],
+                 common_name=j['HALTELIST']['KORTENAAM'][i],
+                 common_city=common_city,
+                 point=pnt)
+        s.save()
+        
+        for k, v in {'zone': 'ZONE'}.items():
+            StopAttribute(stop=s, key=k, value=j['HALTELIST'][v][i]).save()
+        
+        for agency_attr in ['ID', 'KORTENAAM', 'CODE', 'ZONE', 'NAAM', 'TONEN', 'BRUGWACHTER', 'LONGITUDE', 'LATITUDE']:
+           AgencyAttribute(stop=s, agency=a, key="CODE", value=j['HALTELIST'][agency_attr][i]).save()
