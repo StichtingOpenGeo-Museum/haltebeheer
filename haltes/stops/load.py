bf65d49a554d7626490b56a4d9d52305cd02c764
Joel Haasnoot <j.haasnoot@deskbookers.com>
2011-11-27 22:02:27 +0100

    Add agency and points to import example

--- haltes/stops/load.py
+++ haltes/stops/load.py
@@ -1,10 +1,15 @@
 import os
-from models import Stop, StopAttribute
+from django.contrib.gis.geos import *
+from models import Stop, StopAttribute, Agency, AgencyAttribute
 
 def run(verbose=True):
     stops = []
+    a = Agency.objects.filter(id=1)
     for stop in stops:
-        s = Stop(common_name="", common_city = "")
+        pnt = Point(954158.1, 4215137.1, srid=32140) # Fix this, change coordinate system
+        s = Stop(common_name="", common_city = "", point=pnt)
         s.save()
         for attr in stop.attributes:
             StopAttribute(stop=s, key="", value="").save()
+        for agency_attr in stop.attributes:
+            AgencyAttribute(stop=s, agency=a, key="", value="").save()
