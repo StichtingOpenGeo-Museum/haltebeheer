9ede1414ca463541b773d80c5f094945bb673cc6
Joel Haasnoot <j.haasnoot@deskbookers.com>
2012-01-10 23:48:51 +0100

    Remove unused imports

--- haltes/stops/views.py
+++ haltes/stops/views.py
@@ -4,9 +4,6 @@ from models import Stop
 from django.shortcuts import render
 from django.db.models import Count
 from django.views.decorators.cache import cache_page
-from haltes.utils.shortcuts import render_to_geojson
-from django.contrib.gis.shortcuts import render_to_text
-from django.http import HttpResponse
 
 # Cache this frontpage often, it's very slow
 @cache_page(60 * 15)
