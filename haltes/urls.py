9b84f5f781f08ab31a195c17ba6a0fda097ec9e2
Joel Haasnoot <joelhaasnoot@gmail.com>
2013-09-22 18:47:42 +0200

    Add Bootstrap 3.0 for sure this time

--- haltes/urls.py
+++ haltes/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.gis import admin
-from django.views.generic.detail import DetailView
 
+from stops.views import HistoryDetailView
 from stops.models import BaseStop, UserStop
 
 admin.autodiscover()
@@ -10,10 +10,9 @@ urlpatterns = patterns('',
     url(r'^$', 'stops.views.home', name='home'),
     url(r'^cities/?$', 'stops.views.cities', name='cities'),
     url(r'^search/(?P<term>[\w]+)?/?$', 'stops.views.search', name='search'),
-    # \w .,-/\(\)\'\`]
     url(r'^stops/(?P<city>.+)/?$', 'stops.views.city_stops', name='city_stops'),
-    url(r'^stop/(?P<pk>[\d]+)/?$', DetailView.as_view(model=BaseStop), name='stop'),
-    url(r'^stop/tpc/(?P<tpc>[\d]{0,8})/?$', DetailView.as_view(model=UserStop, slug_field='tpc', slug_url_kwarg='tpc'), name='stop_tpc'),
+    url(r'^stop/(?P<pk>[\d]+)/?$', HistoryDetailView.as_view(model=BaseStop), name='stop'),
+    url(r'^stop/tpc/(?P<tpc>[\d]{0,8})/?$', HistoryDetailView.as_view(model=UserStop, slug_field='tpc', slug_url_kwarg='tpc'), name='stop_tpc'),
     url(r'^stop/(?P<stop_id>[\w]+)/json/?$', 'stops.views.stop_json', name='stop_json'),
     
     (r'^admin/', include(admin.site.urls)),
